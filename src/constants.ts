5d8aa748c441dc7144dedb40e7a131aab5643e48
admin7654321 <admin7654321@example.com>
2026-06-20 01:03:12 +0300

    OTA Update 1.0.90

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const OTA_VERSION = '1.0.89';
+export const OTA_VERSION = '1.0.90';
 export const APK_VERSION = '1.0.38';
 
 // ── أدوار المستخدمين ──────────────────────────────────────────────────────────
