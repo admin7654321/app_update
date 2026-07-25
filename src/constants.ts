51cbc00237aa36553b0d692022edc96df514ae4b
admin7654321 <admin7654321@example.com>
2026-07-25 03:35:34 +0300

    New APK Release 1.0.47

--- src/constants.ts
+++ src/constants.ts
@@ -2,7 +2,7 @@ import { App } from '@capacitor/app';
 import { Capacitor } from '@capacitor/core';
 
 export const OTA_VERSION = '1.0.126';
-export const APK_VERSION = '1.0.46';
+export const APK_VERSION = '1.0.47';
 
 /**
  * دالة جلب إصدار الـ APK الحقيقي من نظام الهواتف مباشرة
