7b4e81145b6cd0264a761d2cf3ba64efdad13dcf
admin7654321 <admin7654321@example.com>
2026-07-25 03:47:23 +0300

    OTA Update 1.0.127

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import { App } from '@capacitor/app';
 import { Capacitor } from '@capacitor/core';
 
-export const OTA_VERSION = '1.0.126';
+export const OTA_VERSION = '1.0.127';
 export const APK_VERSION = '1.0.47';
 
 /**
