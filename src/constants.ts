15d7a3022eb490367732327cc18f70c39bb1314b
admin7654321 <admin7654321@example.com>
2026-08-02 21:21:13 +0300

    OTA Update 1.0.180

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import { Capacitor } from '@capacitor/core';
 import { CapacitorUpdater } from '@capgo/capacitor-updater';
 import { Preferences } from '@capacitor/preferences';
 
-export const OTA_VERSION = '1.0.179';
+export const OTA_VERSION = '1.0.180';
 export const APK_VERSION = '1.0.52';
 
 /**
